b382906bd4e065b700d149635d55d97b2384e6a9
Isaac Vono <isaacvono@outlook.com>
2023-11-27 23:54:52 -0300

    with-context-manager-studies

---
+++ concepts/con8.py
@@ -0,0 +1,9 @@
+# Concept: Using with context manager in other scenario and some other new little things.
+
+date = input("Enter today's date: ")
+mood = input("How do you rate your mood today from 1 to 10? ")
+thoughts = input("Let you thoughts flow:\n")
+
+with open(f"../journal/{date}.txt", "w") as file:
+    file.write(f"Your mood was: {mood}/10." + 2 * "\n")
+    file.write(thoughts)
