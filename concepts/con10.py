8390a98e6bcced03ac744a870e8df370666cbffa
Isaac Vono <isaacvono@outlook.com>
2023-11-29 21:01:55 -0300

    new-concept-try-except-error-handling

---
+++ concepts/con10.py
@@ -0,0 +1,13 @@
+# Concept: Difference between if conditionals and try-except for correcting error.
+
+try:
+    width = float(input("Enter rectangle width: "))
+    length = float(input("Enter rectangle length: "))
+
+    if width == length:
+        exit("That looks like a square.")
+
+    area = width * length
+    print(area)
+except ValueError:
+    print("Please enter a number.")
