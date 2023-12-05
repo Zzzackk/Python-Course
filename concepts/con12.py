171dd6f15694811e9e273d64f903af333dd367d5
Isaac Vono <isaacvono@outlook.com>
2023-12-05 18:04:34 -0300

    concept-decoupling-function-output

---
+++ concepts/con12.py
@@ -0,0 +1,18 @@
+# Concept: Decoupling the code to get clean and concise output from functions instead of a whole string, for example.
+def convert(feet_inches):
+    parts = feet_inches.split(" ")
+    feet = float(parts[0])
+    inches = float(parts[1])
+
+    meters = feet * 0.3048 + inches * 0.0254
+    return meters
+
+
+feet_inches = input("Enter feet and inches: ")
+
+result = convert(feet_inches)
+
+if result < 1:
+    print("Kid is too small")
+else:
+    print("Kid can use the slide.")
